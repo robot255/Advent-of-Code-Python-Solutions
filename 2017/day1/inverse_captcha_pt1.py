1f2344afa26ebf1ec46ab526ca3c5001ea8ca7d6
robot255 <34220458+robot255@users.noreply.github.com>
2017-12-04 07:19:58 -0500

    Update inverse_captcha_pt1.py

--- 2017/day1/inverse_captcha_pt1.py
+++ 2017/day1/inverse_captcha_pt1.py
@@ -1,3 +1,15 @@
+def inverse_captcha(input):
+    total = 0
 
-if __name__ == "__main__":
-    pass
+    for idx, i in enumerate(input):
+        if i == input[(idx + 1) % len(input)]:
+            total += int(i)
+
+    return total
+
+
+if __name__ == '__main__':
+    f = open("input.txt", "r")
+    content = f.read()
+    results = inverse_captcha(content)
+    print("Solution: {0}".format(results))
